8134ed0672814be728b0831afc8146d8e206c0dd
rhetherington26 <rhetheri@chicagobooth.edu>
2020-10-15 16:10:24 +0000

    1st Commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
 
+ get("/square/results", { :controller => "application", :action => "results"})
+
+ get("/square/new", { :controller => "application", :action => "blank_square_form"})
+
+ get("/square_root/new", { :controller => "application", :action => "blank_square_root_form"})
+
 end
